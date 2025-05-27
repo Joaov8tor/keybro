2da67b18fa0c43054d135eb0a039018c0faa68fc
Joaov8tor <163661591+Joaov8tor@users.noreply.github.com>
2025-05-27 15:24:18 -0300

    Update key.lua

--- key.lua
+++ key.lua
@@ -31,7 +31,7 @@ TextBox.Position = UDim2.new(0.1, 0, 0.3, 0)
 TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextBox.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
 TextBox.TextSize = 18
-TextBox.PlaceholderText = "Insira a chave"
+TextBox.PlaceholderText = "Insira a key"
 
 Button.Parent = Frame
 Button.Size = UDim2.new(0.8, 0, 0.2, 0)
